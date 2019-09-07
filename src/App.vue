dae29fef25d1c621a62c645372048f8382f0248c
Ángel Cachón <tellme@angelcachon.es>
2019-09-07 21:14:37 +0200

    First commit, everything blank

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>

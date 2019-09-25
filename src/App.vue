6b60b4d8980bc99eafcc35ea38d157619fd2ab3d
Ángel Cachón <tellme@angelcachon.es>
2019-09-25 17:30:30 +0200

    Fixed ordering algorithm and using real-time data in votations

--- src/App.vue
+++ src/App.vue
@@ -47,45 +47,94 @@ export default {
   },
  
   created() {
-    // var referral = window.document.referrer
-    // // console.log("referral", referral)
-    // window.localStorage.setItem("referral", referral)
+    //Here I make things before mounting the app component
+  },
+  mounted() {
+
+      //Load data from the data file and configuration
+      fetch("./data/data.json") // <-- data file in json format
+      .then(res => res.json())
+      .then(json => {
+        //pullate the data objects of the application
+        //Data related to the publication
+        this.newspaper = json.newspaper
+
+        //Data related to de items of the publication
+        this.list = json.list
+
+        //Data about recommendation widget
+        this.teInteresa = json.teInteresa
+
+        //Data about cover, indeed it's not properly coded yet
+        this.cover = json.cover
+
+        //Method that cofigure main colors
+        this.redefiningColors()
+
+        //In case the gallery is a ranking, it look for the votes that must be in a Database, the method is lookingForVotes
+        if(this.newspaper.ordered) this.lookingForVotes()
+      });
+
   },
   methods: {
-    order(list) {
+    order(list) {   //Just return the list of items ordered by votes
       let orderedList = list.sort(function(a, b) {
-        if (a.votes > b.votes) {
+        if (parseInt(a.votes) > parseInt(b.votes)) {
           return -1;
         }
-        if (a.votes < b.votes) {
+        if (parseInt(a.votes) < parseInt(b.votes)) {
           return +1;
         }
         return 0;
       });
-      // console.log("ordenados", orderedList)
       return orderedList;
     },
-    oneMore(item) {
+    oneMore(item) {   // This method add 1 vote to the item desired and send the new value to the DB
+
+      // First I need to locate my item in the internal list
       let myItem = this.list.findIndex(function(el) {
-        
         return item === el.title;
       });
 
         console.log("item ", item, myItem)
-      
-      this.list[myItem].votes = parseInt(this.list[myItem].votes) + 1;
-      console.log("Id: ",this.list[myItem].votesId, this.list[myItem].votes);
+      // Build the URL to make the query to de DB
       let urlAPI = this.newspaper.db_server + "db.php?id="+this.list[myItem].votesId;
+
+        console.log("item ", item, this.list[myItem].votesId)
+
+      // I send the AJAX request to see the actual number the votes
       fetch(urlAPI, {
-          method: 'PUT',
-          
-          body: JSON.stringify({
-            votes: parseInt(this.list[myItem].votes)
-          })
+          method: 'GET'
         })
-        .then(res => res.text()) // OR res.json()
-                      .then(res => console.log(res))
+        .then(res => res.json())
+        .then(res => {
+          console.log("res", res[0].votes)
+          
+          //Then I add a vote to the real number of votes
+          this.list[myItem].votes = parseInt(res[0].votes) + 1;
+          console.log("Id: ",this.list[myItem].votesId, this.list[myItem].votes);
+
+          // Then I send the AJAX request to add the vote
+          fetch(urlAPI, {
+              method: 'PUT',
+              body: JSON.stringify({
+                votes: parseInt(this.list[myItem].votes)
+              })
+            })
+            .then(res => res.text())
+            .then(res => {console.log(res)
+            this.lookingForVotes()
+            })
+                        
+
+        }
+        
+        
+        )
                     
+      
+      
+      
        
     },
     
@@ -111,24 +160,6 @@ export default {
       .then(res => res.json())
       .then(json => {
         console.log("votes", json.length )
-        // if(!json){
-        //   console.log("voy a meter 0 votos a todos por error 9999 ")
-        //           this.list.map(item => {
-        //               item.votes = 0
-        //               // fetch(this.newspaper.db_server + "api2.php/records/mejorfoto", {
-        //               fetch(this.newspaper.db_server + "db.php", {
-        //                 method: 'POST',
-        //                 headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'},
-        //                 body: JSON.stringify({
-        //                   folder: this.newspaper.folder,
-        //                   item: item.img_file,
-        //                   votes: 0
-        //                 })
-        //               })
-        //               .then(res => res.text()) // OR res.json()
-        //               .then(res => console.log(res))
-        //             })
-        //   }
         if(json.length){
           this.votes = json;
           this.list.map(el => {
@@ -169,33 +200,6 @@ export default {
       
     }
     
-  },
-  mounted() {
-
-      //Load data from the data file and configuration
-      fetch("./data/data.json") // <-- data file in json format
-      .then(res => res.json())
-      .then(json => {
-        //pullate the data objects of the application
-        //Data related to the publication
-        this.newspaper = json.newspaper
-
-        //Data related to de items of the publication
-        this.list = json.list
-
-        //Data about recommendation widget
-        this.teInteresa = json.teInteresa
-
-        //Data about cover, indeed it's not properly coded yet
-        this.cover = json.cover
-
-        //Method that cofigure main colors
-        this.redefiningColors()
-
-        //In case the gallery is a ranking, it look for the votes that must be in a Database, the method is lookingForVotes
-        if(this.newspaper.ordered) this.lookingForVotes()
-      });
-
   }
   
 }
